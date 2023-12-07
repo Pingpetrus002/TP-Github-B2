e49a8775cf790cc4db31ad18bef11f81acb29769
Outraged_Flygon <torikian.khoren69@gmail.com>
2023-12-07 12:51:22 +0100

    écriture programme

---
+++ dev_Khoren.py
@@ -0,0 +1,18 @@
+import random
+
+def lancer_de():
+    return random.randint(1, 6)
+
+def main():
+    nombre_de_lancers = 5
+    somme_points = 0
+
+    for _ in range(nombre_de_lancers):
+        resultat_lancer = lancer_de()
+        print(f'Résultat du lancer : {resultat_lancer}')
+        somme_points += resultat_lancer
+
+    print(f'\nNombre total de points : {somme_points}')
+
+if __name__ == "__main__":
+    main()
