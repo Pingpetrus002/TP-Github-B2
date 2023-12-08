a44e31b5f312ac266b0ae5106bc58fb692e527af
Maxoubiloux <max.kovacevic@laposte.net>
2023-12-08 11:15:07 +0100

    Create filtrage-tache.py

---
+++ filtrage-tache.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import locale
+
+locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
+
+date_string = input("Entrez une date au format jj/mm/yyyy : ")
+
+try:
+    date = datetime.strptime(date_string, "%d/%m/%Y")
+
+    nom_jour = date.strftime("%A")
+    
+    print(f"Le jour correspondant à la date {date_string} est : {nom_du_jour}")
+    
+except ValueError:
+    print("Format de date incorrect. Assurez-vous d'utiliser le format jj/mm/yyyy.")
